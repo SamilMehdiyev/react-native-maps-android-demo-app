5fee8c8921881e2a6cfda32eff9d08f6f4b95cb1
Shamil Mehdiyev <samil.mehdiyev@gmail.com>
2019-04-13 01:08:35 +0400

    First commit

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    flex: 1
+  }
+});
